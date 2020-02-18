a13e7dd94f1ceea665e4a183e62f63b843481ef9
manginalisson <mangin.alisson@gmail.com>
2020-02-18 15:19:03 +0100

    ajout component weather

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>YOLO</h1>
+        <Weather/>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Weather() {
+    return (
+        <div>
+            <h1> YOLO MÉTÉO</h1>
+        </div>
+    )
+}
+
+export default Weather
