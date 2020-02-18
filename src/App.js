6ccb5565d2fba7bb109fc75a6d199f69a6702d7e
manginalisson <mangin.alisson@gmail.com>
2020-02-18 15:22:12 +0100

    soleil

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Weather/>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="sun.png" className="App-logo" alt="logo" />
       </header>
     </div>
   );
