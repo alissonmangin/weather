3d262ee12a2e11ae9c18943e4a678be7b7c926a2
manginalisson <mangin.alisson@gmail.com>
2020-02-19 10:21:13 +0100

    opti URL API

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const baseURL = "https:api.openweathermap.org/data/2.5";
+const appID = "&appid=55a07ce7dd61dbb72f56b25ce2b495df";
+
 export function getWeather(){
-    return axios.get("https:api.openweathermap.org/data/2.5/weather?q=Nancy&appid=55a07ce7dd61dbb72f56b25ce2b495df");
+    return axios.get(baseURL+"/weather?q=Nancy"+appID);
 }
